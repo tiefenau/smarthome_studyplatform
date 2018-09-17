150f0d3047e3ae2eb4b70e9c054f3c25df7710dd
Rahul Lao <rahullao@gmail.com>
2018-09-18 00:20:00 +0200

    UPDATE: general configuration template

--- angular_workspace/pfiva-web/src/app/configuration-main/general-configuration/general-configuration.component.ts
+++ angular_workspace/pfiva-web/src/app/configuration-main/general-configuration/general-configuration.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general-configuration.component.css']
 })
 export class GeneralConfigurationComponent implements OnInit {
-
+  isFeedbackQuery: boolean = false;
+  isServerKey: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleQueriesHotword() {
+    console.log("hotword toggle")
+  }
+
+  toggleFeedbackQuery() {
+    this.isFeedbackQuery = !this.isFeedbackQuery;
+  }
+
+  toggleServerKey() {
+    this.isServerKey = !this.isServerKey;
   }
 
 }
