4c08c4cd1106657215f49ed21ad8757834befe56
Rahul Lao <rahullao@gmail.com>
2018-09-24 02:17:53 +0200

    UPDATE: data model for survey with user response

---
+++ workspace/data-model/src/main/java/de/pfiva/data/model/survey/SurveyResponseData.java
@@ -0,0 +1,14 @@
+package de.pfiva.data.model.survey;
+
+public class SurveyResponseData {
+
+	private Survey survey;
+
+	public Survey getSurvey() {
+		return survey;
+	}
+
+	public void setSurvey(Survey survey) {
+		this.survey = survey;
+	}
+}
