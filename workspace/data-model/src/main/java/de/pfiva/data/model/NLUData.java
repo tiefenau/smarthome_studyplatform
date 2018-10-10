fac218cd2b85c326523e4b3cf3ac585539f71057
Rahul Lao <rahullao@gmail.com>
2018-10-11 00:03:03 +0200

    UPDATE: nludata model has user field

--- workspace/data-model/src/main/java/de/pfiva/data/model/NLUData.java
+++ workspace/data-model/src/main/java/de/pfiva/data/model/NLUData.java
@@ -10,6 +10,7 @@ public class NLUData implements Serializable {
 	
 	private SnipsOutput snipsOutput;
 	private Feedback feedback;
+	private User user;
 	
 	public SnipsOutput getSnipsOutput() {
 		return snipsOutput;
@@ -23,8 +24,14 @@ public class NLUData implements Serializable {
 	public void setFeedback(Feedback feedback) {
 		this.feedback = feedback;
 	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
 	@Override
 	public String toString() {
-		return "NLUData [snipsOutput=" + snipsOutput + ", feedback=" + feedback + "]";
+		return "NLUData [snipsOutput=" + snipsOutput + ", feedback=" + feedback + ", user=" + user + "]";
 	}
 }
