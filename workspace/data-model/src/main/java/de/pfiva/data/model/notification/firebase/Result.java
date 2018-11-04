40f016c5279968ca3d38bc688911765a69027396
Rahul Lao <rahullao@gmail.com>
2018-11-05 00:57:19 +0100

    UPDATE: refactor firebase response model

--- workspace/data-model/src/main/java/de/pfiva/data/model/notification/firebase/Result.java
+++ workspace/data-model/src/main/java/de/pfiva/data/model/notification/firebase/Result.java
@@ -3,6 +3,7 @@ package de.pfiva.data.model.notification.firebase;
 public class Result {
 
 	private String error;
+	private String message_id;
 
 	public String getError() {
 		return error;
@@ -10,5 +11,13 @@ public class Result {
 
 	public void setError(String error) {
 		this.error = error;
+	}
+
+	public String getMessage_id() {
+		return message_id;
+	}
+
+	public void setMessage_id(String message_id) {
+		this.message_id = message_id;
 	}
 }
