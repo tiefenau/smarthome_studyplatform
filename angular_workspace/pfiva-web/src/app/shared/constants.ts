3c9313eb4bddf61d0df23418c49ea4ca8b555aca
Rahul Lao <rahullao@gmail.com>
2018-12-10 13:56:55 +0100

    UPDATE: base url updation

--- angular_workspace/pfiva-web/src/app/shared/constants.ts
+++ angular_workspace/pfiva-web/src/app/shared/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static readonly PFIVA_BASE_URL: string = "http://localhost:9001/data/ingestion";
+    public static readonly PFIVA_BASE_URL: string = "http://future-iot.de/data/ingestion";
     public static readonly PFIVA_NLU_DATA: string = "/nlu-data";
     public static readonly PFIVA_USERS: string = "/users";
     public static readonly PFIVA_ADD_USER: string = "/user";
